383a60be3741d77bc48bba78675b3fff534c6a50
phuocvinh143 <maiphuocvinh143@gmail.com>
2019-10-02 00:09:49 +0700

    no message

--- CTDL_TREE_POINTER.cpp
+++ CTDL_TREE_POINTER.cpp
@@ -36,6 +36,14 @@ Node Create2New(DataType v, Tree left, Tree right) {
     return n;
 }
 
+Node Create3(DataType v, Tree first, Tree second, Tree third){
+    Node n = (Node)malloc(sizeof(Node));
+    n->Data = v;
+    n->Leftmost_Child = first;
+    if (first != NULL) first->Right_Sibling = second;
+    if (second != NULL) second->Right_Sibling = third;
+}
+
 void PreOrder(Node n){
     cout << n->Data << " ";
     Node i = n->Leftmost_Child;
@@ -83,6 +91,21 @@ void BT2(Tree T){
     }
 }
 
+void VD(){
+    Tree T1, T2, T3, T4, T5, T;
+    T1 = Create2New(6, NULL, NULL);
+    T2 = Create2New(5, NULL, NULL);
+    T3 = Create2New(4, NULL, T2);
+    T4 = Create2New(3, T1, NULL);
+    T5 = Create2New(2, T3, T4);
+    T = Create2New(1, T5, NULL);
+    PreOrder(T);
+    cout << endl;
+    InOrder(T);
+    cout << endl;
+    PosOrder(T);
+}
+
 void BT1(){
     Tree T[15];
     T[0] = Create2New('N', NULL, NULL);
@@ -109,21 +132,29 @@ void BT1(){
     BT2(T[13]);
 }
 
-
+void BT5(){
+    Tree T[15];
+    T[0] = Create3('L', NULL, NULL, NULL);
+    T[1] = Create3('M', NULL, NULL, NULL);
+    T[2] = Create3('I', T[0], T[1], NULL);
+    T[3] = Create3('D', T[2], NULL, NULL);
+    T[4] = Create3('G', NULL, NULL, NULL);
+    T[5] = Create3('H', NULL, NULL, NULL);
+    T[6] = Create3('J', NULL, NULL, NULL);
+    T[7] = Create3('C', T[4], T[5], T[6]);
+    T[8] = Create3('K', NULL, NULL, NULL);
+    T[9] = Create3('E', NULL, NULL, NULL);
+    T[10] = Create3('F', T[8], NULL, NULL);
+    T[11] = Create3('B', T[9], T[10], NULL);
+    T[12] = Create3('B', T[11], T[7], T[3]);
+    PreOrder(T[12]);
+    cout << endl;
+    InOrder(T[12]);
+    cout << endl;
+    PosOrder(T[12]);
+    cout << endl;
+}
 
 int main(){
-    BT1();
-//    Tree T1, T2, T3, T4, T5, T;
-//    T1 = Create2New(6, NULL, NULL);
-//    T2 = Create2New(5, NULL, NULL);
-//    T3 = Create2New(4, NULL, T2);
-//    T4 = Create2New(3, T1, NULL);
-//    T5 = Create2New(2, T3, T4);
-//    T = Create2New(1, T5, NULL);
-//    PreOrder(T);
-//    cout << endl;
-//    InOrder(T);
-//    cout << endl;
-//    PosOrder(T);
-
+    BT5();
 }

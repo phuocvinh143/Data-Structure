fa761f9cd1d5b2fa66e6a6836ef1d69bbc60f6cb
phuocvinh143 <maiphuocvinh143@gmail.com>
2019-10-02 19:15:24 +0700

    no message

--- CTDL_BINARY_SEARCH_TREE.cpp
+++ CTDL_BINARY_SEARCH_TREE.cpp
@@ -19,9 +19,9 @@ Tree Search(KeyType x, Tree Root) {
     if (Root->Key < x) return Search(x, Root->right);
 }
 
-Tree Insert_Node(KeyType x, Tree& Root) {
+void Insert_Node(KeyType x, Tree& Root) {
     if (Root == NULL) {
-        Root = (Node*)malloc(sizeof(Node));
+        Root = new Node;
         Root->Key = x;
         Root->left = NULL;
         Root->right = NULL;
@@ -30,7 +30,7 @@ Tree Insert_Node(KeyType x, Tree& Root) {
     else if (x < Root->Key) Insert_Node(x, Root->left);
 }
 
-KeyType DeleteMin(Tree Root) {
+KeyType DeleteMin(Tree& Root) {
     KeyType k;
     if (Root->left == NULL) {
         k = Root->Key;
@@ -40,16 +40,18 @@ KeyType DeleteMin(Tree Root) {
     return DeleteMin(Root->left);
 }
 
-void Delete_Node(KeyType x, Tree Root){
+void Delete_Node(KeyType x, Tree& Root){
     if (Root != NULL) {
         if (x < Root->Key) Delete_Node(x, Root->left);
         else if (x > Root->Key) Delete_Node(x, Root->right);
         else {
-            if (Root->left == NULL && Root->right == NULL) Root == NULL;
+            if ((Root->left == NULL) && (Root->right == NULL)) Root = NULL;
             else {
                 if (Root->left == NULL) Root = Root->right;
                 else if (Root->right == NULL) Root = Root->left;
-                else Root->Key = DeleteMin(Root->right);
+                else {
+                    Root->Key = DeleteMin(Root->right);
+                }
             }
         }
     }
@@ -62,9 +64,34 @@ void PreOrder(Node* T) {
     PreOrder(T->right);
 }
 
+void InOrder(Node* T) {
+    if (T == NULL) return;
+    InOrder(T->left);
+    cout << T->Key << " ";
+    InOrder(T->right);
+}
+
+void PosOrder(Node* T) {
+    if (T == NULL) return;
+    PosOrder(T->left);
+    PosOrder(T->right);
+    cout << T->Key << " ";
+}
+
 int main(){
     freopen("input.txt", "r", stdin);
     freopen("output.txt", "w", stdout);
+    int n; cin >> n;
+    Tree T = NULL;
+    KeyType root;
+    for (int i = 1; i <= n; ++i) {
+        int x; cin >> x;
+        if (i == 1) {
+            root = x;
+        }
+        Insert_Node(x, T);
+    }
+    PreOrder(Search(root, T));
 }
 
 
---
+++ CTDL_BINARY_SEARCH_TREE_string.cpp
@@ -0,0 +1,99 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+typedef string KeyType;
+
+typedef struct Node{
+    KeyType Key;
+    Node* left;
+    Node* right;
+};
+
+typedef Node* Tree;
+
+Tree Search(KeyType x, Tree Root) {
+    if (Root == NULL) return NULL;
+    if (Root->Key == x) return Root;
+    if (Root->Key > x) return Search(x, Root->left);
+    if (Root->Key < x) return Search(x, Root->right);
+}
+
+void Insert_Node(KeyType x, Tree& Root) {
+    if (Root == NULL) {
+//        string có thể hiểu là một class gồm nhiều toán tử, hàm được xây dựng trên nó nên khi
+//        sử dụng cần phải khởi tạo cấu trúc ban đầu cho string -> mà "malloc" thì không hỗ trợ
+//        khởi tạo constructor nên phải dùng "new"
+        Root = new Node;
+
+        Root->Key = x;
+        Root->left = NULL;
+        Root->right = NULL;
+    }
+    else if (x > Root->Key) Insert_Node(x, Root->right);
+    else if (x < Root->Key) Insert_Node(x, Root->left);
+}
+
+KeyType DeleteMin(Tree& Root) {
+    KeyType k;
+    if (Root->left == NULL) {
+        k = Root->Key;
+        Root = Root->right;
+        return k;
+    }
+    return DeleteMin(Root->left);
+}
+
+void Delete_Node(KeyType x, Tree& Root){
+    if (Root != NULL) {
+        if (x < Root->Key) Delete_Node(x, Root->left);
+        else if (x > Root->Key) Delete_Node(x, Root->right);
+        else {
+            if ((Root->left == NULL) && (Root->right == NULL)) Root = NULL;
+            else {
+                if (Root->left == NULL) Root = Root->right;
+                else if (Root->right == NULL) Root = Root->left;
+                else {
+                    Root->Key = DeleteMin(Root->right);
+                }
+            }
+        }
+    }
+}
+
+void PreOrder(Node* T) {
+    if (T == NULL) return;
+    cout << T->Key << "\n";
+    PreOrder(T->left);
+    PreOrder(T->right);
+}
+
+void InOrder(Node* T) {
+    if (T == NULL) return;
+    InOrder(T->left);
+    cout << T->Key << "\n";
+    InOrder(T->right);
+}
+
+void PosOrder(Node* T) {
+    if (T == NULL) return;
+    PosOrder(T->left);
+    PosOrder(T->right);
+    cout << T->Key << "\n";
+}
+
+int main(){
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    int n; cin >> n;
+    Tree T = NULL;
+    for (int i = 1; i <= n; ++i) {
+        string s; cin >> s;
+        Insert_Node(s, T);
+    }
+    InOrder(Search("HAIPHONG",T));
+}
+
+
+
+
